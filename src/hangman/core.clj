dc7d87f4abee22837f66989fa8eb136eb38c408f
Jozef Wagner <jozef.wagner@gmail.com>
2012-02-05 23:28:41 +0100

    solver

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -11,5 +11,62 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns hangman.core
-  "Core Hangman functions")
+  "Core Hangman functions.")
+
+;;;; STEP 1: initialize profiles
+
+(comment
+
+  ;; each profile contains:
+  ;; - current game
+  ;; - number of games won
+  ;; - number of games played
+  ;; each game contains:
+  ;; - word to guess
+  ;; - guesses seq
+  ;; it is good to represent a profile as a map
+  {:won 3
+   :all 5
+   :game {:word "antelope"
+          :guesses [\f \l \c \d]}}
+
+  ;; we want to support multiple users, each having
+  ;; its own profile
+  {"Peter" {:won 3
+            :all 3} ; does not have active game
+   "Mark" {:won 2
+           :all 3
+           :game {:word "caribou"}} ; game started but not yet guessed
+   "Rebecca" {:won 0
+              :all 3
+              :game {:word "sausage"
+                     :guesses [\t \i \n]}}} ; guessing right now
+  
+  )
+
+;; profiles are maps indexed by user names
+(def profiles (atom {}))
+
+(comment
+
+  ;; obtain actual value
+  @state
+  
+  )
+
+;;;; STEP 2: get users profile
+
+(defn get-profile
+  "Returns profile for a given user name. Can be nil."
+  [user-name]
+  (@profiles user-name))
+
+;;;; start new game
+
+;; old game in progress, add to all games
+;; if won, add to won games
+
+;;;; new guess
+
+;; add next guess to the current game
 
---
+++ src/hangman/solver.clj
@@ -0,0 +1,121 @@
+;; Copyright (C) 2012, Jozef Wagner.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0
+;; (http://opensource.org/licenses/eclipse-1.0.php) which can be found
+;; in the file epl-v10.html at the root of this distribution.
+;;
+;; By using this software in any fashion, you are agreeing to be bound
+;; by the terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
+
+(ns hangman.solver
+  "Match guesses.")
+
+;;;; STEP 1: match word with chosen characters
+
+(comment
+
+  ;; a word
+  "hangman"
+
+  ;; word converted to seq of chars
+  (seq "hangman")
+
+  ;; go through the word and check if characters are equal
+  (map #(= % \a) "hangman")
+  
+  ;; we can use set to check for equality
+  (map #{\a} "hangman")
+
+  ;; we can put more chars into set
+  (map #{\a \h \c} "hangman")
+
+  ;; replace nils with underscore character
+  (let [matches (map #{\a \h \c} "hangman")]
+    (replace {nil \_} matches))
+
+  ;; put it all together
+  (let [replace-map #{\a \h \c}
+        replace-fn (fn [char] (if (replace-map char)
+                               char
+                               \_))]
+    (map replace-fn "hangman"))
+
+  ;; convert seq of chars back to string
+  (str \h \a \n \g \m \a \n)
+  
+  )
+
+(defn match
+  "Returns partially revealed word, based on guesses stored in
+  chars seq. Not yet guessed characters are replaced with underscore
+  character."
+  [word guesses]
+  (let [guess-set (set guesses)
+        replace-fn #(if (guess-set %) % \_)
+        matched-seq (map replace-fn word)]
+    (apply str matched-seq)))
+
+(comment
+
+  ;; test match fn
+  (match "hangman" [\h \n \c])
+
+  ;; test if no guess characters are given
+  (match "hangman" nil)
+
+  )
+
+;;;; STEP 2: obtain wrong guesses
+
+(comment
+
+  ;; removes guessed characters from a word
+  (remove (set "hangman") #{\h \c \n})
+
+  ;; convert seq of wrong guesses to a set, in order to remove
+  ;; duplicate guesses
+  (set (remove (set "hangman") [\h \c \a \c]))
+
+  )
+
+(defn wrong-guesses
+  "Returns seq of wrong guesses for a given word.
+  Returns empty seq if there are no wrong guesses."
+  [word guesses]
+  (remove (set word) guesses))
+
+(comment
+
+  ;; test wrong-guesses fn
+  (wrong-guesses "hangman" [\h \a \c \c])
+
+  (wrong-guesses nil [\h])
+
+  (wrong-guesses "hangman" nil)
+  
+  )
+
+;;;; STEP 3: check if game is over
+
+(defn won?
+  "Returns true if guesses contains all characters from word.
+  Returns nil otherwise."
+  [word guesses]
+  (let [guess-set (set guesses)]
+    (empty? (remove guess-set word))))
+
+(comment
+
+  ;; test won? fn
+  (won? "hangman" [\h \a \n \g \c])
+
+  (won? "hangman" [\h \a \n \g \m \c])
+
+  (won? nil [\h])
+
+  (won? "hangman" nil)
+
+  )
